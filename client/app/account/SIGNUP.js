10aeb09f326c6968b3f38d24a39d1ee7a368cf46
jamalsiam <jamalsiam1995@gmail.com>
2017-07-16 19:20:03 +0300

    first commit

--- client/app/account/SIGNUP.js
+++ client/app/account/SIGNUP.js
@@ -1,7 +1,43 @@
 angular.module('signup' , [])
 
-.controller('signupController', function ($scope , $window , $location  /*,aa*/) {
+.controller('signupController', function ($scope , $window , $location  ,Signup) {
+	$scope.signUp=function(firstName,lastName,email,password){
+		if(firstName && lastName && email && password){
+			Signup.signUp({firstName:firstName,
+							lastName:lastName,
+							email:email,
+							password:password})
+			then(function(i){
+				if(i=="checkemail"){
+					$scope.msg="email used before"
+				}
+				else
+				{
+					///// go to new location and save in storage
+				}
+			})
 
+
+		}//if (is not epity) 
+		else
+		{
+
+
+
+		}//else
+
+	}///siginUp
 });
 
 
+				
+
+
+
+
+
+
+
+
+
+							
--- client/app/services.js
+++ client/app/services.js
@@ -1,7 +1,18 @@
 angular.module('u.services', [])
 
-// get all book
-.factory('book', function ($http) {
+.factory('Signup', function ($http) {
+  var signUp= function (signup) {
+  console.log(signup);
+    return $http({
+      method: 'POST',
+      url: '/api/signup',
+      data: signup
+    }).then(function (res) {
+      return res.data;
+    });
+  };
 
-
+  return {
+    signUp:signUp
+  }
 })
